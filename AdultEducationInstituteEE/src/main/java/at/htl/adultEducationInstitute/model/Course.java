acdb36dfc2683243388c84cba50212b9a335d9e4
Maria Milic <maria@MacBook-Pro-od-Maria.local>
2019-10-22 14:05:21 +0200

    Course-File modified

--- AdultEducationInstituteEE/src/main/java/at/htl/adultEducationInstitute/model/Course.java
+++ AdultEducationInstituteEE/src/main/java/at/htl/adultEducationInstitute/model/Course.java
@@ -1,4 +1,46 @@
 package at.htl.adultEducationInstitute.model;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Course {
+
+    private Long courseID;          //KursID
+    private String courseName;      //Kursname
+    private int amountBookings;     //Anzahl von Buchungen
+
+    public Course() {
+    }
+
+    public Course(Long courseID, String courseName, int amountBookings) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.amountBookings = amountBookings;
+    }
+
+    public Long getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(Long courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getAmountBookings() {
+        return amountBookings;
+    }
+
+    public void setAmountBookings(int amountBookings) {
+        this.amountBookings = amountBookings;
+    }
 }

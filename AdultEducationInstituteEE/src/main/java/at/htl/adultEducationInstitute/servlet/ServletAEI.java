a0b1bab804008175756ee6d0926d5904d3b7a840
Maria Milic <maria@MacBook-Pro-od-Maria.local>
2019-10-23 12:15:48 +0200

    Servlet-File added

--- AdultEducationInstituteEE/src/main/java/at/htl/adultEducationInstitute/servlet/ServletAEI.java
+++ AdultEducationInstituteEE/src/main/java/at/htl/adultEducationInstitute/servlet/ServletAEI.java
@@ -1,4 +1,18 @@
 package at.htl.adultEducationInstitute.servlet;
 
-public class ServletAEI {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("aeiServlet")
+public class ServletAEI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
 }

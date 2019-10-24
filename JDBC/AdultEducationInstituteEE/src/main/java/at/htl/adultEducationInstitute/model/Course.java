9bedf5f0ad8d2d4431ea5ed8ed77238a21a1c895
Maria Milic <maria@MacBook-Pro-od-Maria.local>
2019-10-24 08:44:14 +0200

    JDBC and JPA  Directories created

---
+++ JDBC/AdultEducationInstituteEE/src/main/java/at/htl/adultEducationInstitute/model/Course.java
@@ -0,0 +1,46 @@
+package at.htl.adultEducationInstitute.model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Course {
+
+    private Long id;          //KursID
+    private String courseName;      //Kursname
+    private int amountBookings;     //Anzahl von Buchungen
+
+    public Course() {
+    }
+
+    public Course(Long id, String courseName, int amountBookings) {
+        this.id = id;
+        this.courseName = courseName;
+        this.amountBookings = amountBookings;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getAmountBookings() {
+        return amountBookings;
+    }
+
+    public void setAmountBookings(int amountBookings) {
+        this.amountBookings = amountBookings;
+    }
+}

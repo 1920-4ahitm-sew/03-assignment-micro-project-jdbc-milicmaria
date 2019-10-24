9678fca57f822c06fbc1af5939c6ac1fdbdee164
Maria Milic <maria@MacBook-Pro-od-Maria.local>
2019-10-24 22:56:55 +0200

    small modifications

--- AdultEducationInstitute/JPAadultEducationInstituteEE/src/main/java/at/htl/adulteducationinstitute/boundary/CourseEndpoint.java
+++ AdultEducationInstitute/JPAadultEducationInstituteEE/src/main/java/at/htl/adulteducationinstitute/boundary/CourseEndpoint.java
@@ -5,11 +5,10 @@ import at.htl.adulteducationinstitute.entity.Course;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.persistence.PostUpdate;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.awt.*;
 import java.util.List;
 
@@ -37,5 +36,37 @@ public class CourseEndpoint {
         return em.createNamedQuery("Course.findAll", Course.class).getResultList();
     }
 
+    @POST
+    public void update(Course course){
+        em.merge(course);
+    }
+
+    @PUT
+    @Path("put{id}")
+    public Response update(@PathParam("id") Long id, Course c) {
+        Course updateCourse = getOne(id);
+        updateCourse.setCourseName(c.getCourseName());
+        updateCourse.setAmoutBookings(c.getAmoutBookings());
+
+        update(updateCourse);
+
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("post")
+    public Response create(Course c) {
+        create(c);
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("delete{id}")
+    public Response delete(@PathParam("id") Long id) {
+        Course c = getOne(id);
+        delete(id);
+
+        return Response.ok().build();
+    }
 
 }

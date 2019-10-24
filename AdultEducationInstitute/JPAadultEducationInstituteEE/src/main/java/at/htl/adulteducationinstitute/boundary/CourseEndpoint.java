a03ffb68291396f536b4b8aed18220196248aaf5
Maria Milic <maria@MacBook-Pro-od-Maria.local>
2019-10-24 22:02:34 +0200

    Organize folders

---
+++ AdultEducationInstitute/JPAadultEducationInstituteEE/src/main/java/at/htl/adulteducationinstitute/boundary/CourseEndpoint.java
@@ -0,0 +1,41 @@
+package at.htl.adulteducationinstitute.boundary;
+
+import at.htl.adulteducationinstitute.entity.Course;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+import java.util.List;
+
+@Stateless
+@Path("/")
+public class CourseEndpoint {
+    @PersistenceContext
+    EntityManager em;
+
+    public void init(){
+        System.out.println("*****COURSEENDPOINT CREATED*****");
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Course getOne(@PathParam("id") long id){
+        return em.find(Course.class, id);
+    }
+
+    @GET
+    @Path("findAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Course> getAll(){
+        return em.createNamedQuery("Course.findAll", Course.class).getResultList();
+    }
+
+
+}
